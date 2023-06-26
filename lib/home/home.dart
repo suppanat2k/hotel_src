d849064b5c3c2d60f0e931a9b7bd5c4d41bb9510
Suppanat <suppanat2k@gmail.com>
2023-06-26 20:50:33 +0700

    hotel commit 26/06/2023

---
+++ lib/home/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeHotel extends StatefulWidget {
+  const HomeHotel({super.key});
+
+  @override
+  State<HomeHotel> createState() => _HomeHotelState();
+}
+
+class _HomeHotelState extends State<HomeHotel> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
